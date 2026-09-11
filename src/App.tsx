ba882dc95eccaa9a1814183b6dd67dc79bffd81b
ismot-jahan <jahanshimla2002@gamil.com>
2026-09-11 23:02:54 +0600

    Hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@
 
 import './App.css'
 import Navbar from './Components/navbar'
+import HeroSection from './Components/hero'
+
 function App() {
   
 
   return (
     <>
       <Navbar></Navbar>
-                 
+       <HeroSection></HeroSection>          
     </>
   )
 }
